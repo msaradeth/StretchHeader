05ec5c6fb8e01049df7adab5b00eb6c53b1fb1be
Mike Saradeth <mikesaradeth@yahoo.com>
2019-05-12 14:28:44 -0700

    stretch header

---
+++ StretchHeader/ListVC.swift
@@ -0,0 +1,97 @@
+//
+//  ListVC.swift
+//  StretchHeader
+//
+//  Created by Mike Saradeth on 5/9/19.
+//  Copyright © 2019 Mike Saradeth. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+    //set up views
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.rowHeight = 80
+        return tableView
+    }()
+    var stretchHeader: StretchHeader
+    var items: [String]
+    
+    
+    init(items: [String?], stretchHeader: StretchHeader) {
+        self.items = items.compactMap{ $0 }.sorted(by: { $0 < $1 })
+        self.stretchHeader = stretchHeader
+        super.init(nibName: nil, bundle: nil)
+        self.view.backgroundColor = .white
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("required init?(coder aDecoder: NSCoder) not implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupHeaderView()
+        setupTableView()
+    }
+
+    
+    private func setupHeaderView() {
+        view.addSubview(stretchHeader)
+        stretchHeader.anchorToSuperView()
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.topAnchor.constraint(equalTo: stretchHeader.bottomAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+
+}
+
+extension ListVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+}
+
+extension ListVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.tableView.bounds.width, height: 150))
+        imageView.image = UIImage(named: "header")
+        imageView.clipsToBounds = true
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        view.backgroundColor = .yellow
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        title = items[indexPath.row]
+    }
+}
+
+extension ListVC: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        stretchHeader.stretch(contentOffset: scrollView.contentOffset)
+    }
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        print("scrollViewDidEndDragging")
+        stretchHeader.setToDefaultHeight()
+    }
+}
---
+++ StretchHeader/StretchHeader.swift
@@ -0,0 +1,76 @@
+//
+//  StretchHeader.swift
+//  StretchHeader
+//
+//  Created by Mike Saradeth on 5/12/19.
+//  Copyright © 2019 Mike Saradeth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StretchHeader: UIView {
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "header"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIView.ContentMode.scaleToFill
+        return imageView
+    }()
+    private var heightConstraint: NSLayoutConstraint!
+    var defaultHeight: CGFloat
+    var maxHeight: CGFloat
+    
+    init(height: CGFloat, maxHeight: CGFloat) {
+        self.defaultHeight = height
+        self.maxHeight = maxHeight
+        super.init(frame: .zero)
+        setupImageView()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("required init?(coder aDedoder: NSCoder) not implemented")
+    }
+
+    //MARK: set up views
+    private func setupImageView() {
+        addSubview(imageView)
+        imageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+
+    func anchorToSuperView() {
+        guard let superview = self.superview else { return }
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        heightConstraint = self.heightAnchor.constraint(equalToConstant: defaultHeight)
+        self.topAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.topAnchor).isActive = true
+        self.leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
+        self.trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true    
+        NSLayoutConstraint.activate([heightConstraint])
+    }
+    
+    
+    //MARK: functions to animate view
+    func stretch(contentOffset: CGPoint) {
+        let newHeight = heightConstraint.constant - contentOffset.y
+        guard newHeight >= defaultHeight && newHeight <= maxHeight else { return }
+            
+        heightConstraint.constant = newHeight
+        layoutIfNeeded()
+    }
+    func setToDefaultHeight() {
+        heightConstraint.constant = defaultHeight
+        animate()
+    }
+    
+    func animate(duration: TimeInterval = 0.4) {
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       options: .curveEaseInOut,
+                       animations: { self.layoutIfNeeded() },
+                       completion: nil)
+    }
+    
+}
